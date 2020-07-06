f03f6207978bb645a4ef530b2d2548e46aa2a868
supercmmetry <vishaals2000@gmail.com>
2020-07-06 15:27:45 +0530

    tests: add more detail
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- tests/adjective_noun.rs
+++ tests/adjective_noun.rs
@@ -3,7 +3,7 @@ use jirachi::Wishable;
 
 #[test]
 fn wish_key() {
-    println!("Wishing for a key...");
+    println!("Wishing for a adjective-noun key...");
     let mut jirachi = Jirachi::new().unwrap();
     println!("wish: {}", jirachi.wish().unwrap());
 }
--- tests/collision_resistance.rs
+++ tests/collision_resistance.rs
@@ -3,7 +3,7 @@ use jirachi::Wishable;
 
 #[test]
 fn wish_key() {
-    println!("Wishing for a key...");
+    println!("Wishing for a collision-resistant key...");
     let mut jirachi = Jirachi::new().unwrap();
     println!("wish: {}", jirachi.wish().unwrap());
 }
